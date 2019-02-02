6709413e16b9293df56839b3edd6e127ac4fb4fa
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-02-02 02:25:55 +0000

    bug: fix typo in db url

--- src/model/index.js
+++ src/model/index.js
@@ -13,7 +13,7 @@ let sequelize;
 
 if (process.env.DATABASE_URL) {
 // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,
+  sequelize = new Sequelize(process.env.DATABASE_URL,
     {
       dialect: 'postgres',
       protocol: 'postgres',
